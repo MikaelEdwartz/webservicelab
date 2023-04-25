afc111cb47ea3b26eeacf804d68125adc7cd796c
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-04-25 18:15:47 +0200

    refactor code

--- Client/src/main/java/se/iths/client/Main.java
+++ Client/src/main/java/se/iths/client/Main.java
@@ -8,53 +8,77 @@ import java.util.Scanner;
 import java.util.ServiceLoader;
 
 public class Main {
-    private static final String CAESAR = "caesar";
-    private static final String ATBASH = "atbash";
     private static final Scanner scanner = new Scanner(System.in);
     private static String input;
 
     public static void main(String[] args) {
         printGreeting();
         input = scanner.nextLine();
+        initializeLoopAndEncryptInput(printCipher());
+    }
 
+    private static void initializeLoopAndEncryptInput(List<String> list) {
         while (true) {
             printMenuOptions();
-            var encryptStrategy = scanner.nextInt();
-
-            switch (encryptStrategy) {
-                case 0 -> System.exit(0);
-                case 1 -> encryptMessage(ATBASH, input);
-                case 2 -> encryptMessage(CAESAR, input);
-                case 9 -> getInput();
-                default -> System.out.println("Invalid command!");
-            }
+            var encryptStrategy = scanner.nextLine();
+            executeCommand(list, encryptStrategy);
         }
+    }
 
+    private static void executeCommand(List<String> list, String encryptStrategy) {
+        if (encryptStrategy.equals("e"))
+            System.exit(0);
+        else if (Integer.parseInt(encryptStrategy) == 0)
+            getInput();
+        else
+            encryptMessage(list.get(Integer.parseInt(encryptStrategy) - 1), input);
     }
 
     private static void printGreeting() {
         System.out.println("What message would you like to encrypt?");
     }
 
-    private static void printMenuOptions() {
+    private static void getPrintln() {
         System.out.println("""
-                |================================================================ |
-                | Which of the following cipher strategies would you like to use? |
-                | 0. To exit program                                              |  
-                | 1. Cipher with the Atbash algorithm.                            |
-                | 2. Cipher with the Caesar algorithm                             |
-                | 9. To cipher another word                                       |
-                |================================================================ |
+                |================================================================
+                | Which of the following cipher strategies would you like to use?
+                | e. To exit program
+                | 0. To cipher another word""");
+    }
+
+    private static void printAnnotations() {
+        var annotations = printCipher();
+        for (int i = 0; i < annotations.size(); i++)
+            System.out.println("| " + (i + 1) + ". Cipher with the " + annotations.get(i) + " algorithm");
+
+    }
+
+    private static void printMenuOptions() {
+        getPrintln();
+        printAnnotations();
+        System.out.println("""                                     
+                |================================================================
                 """);
+
     }
 
     private static void getInput() {
+
         input = scanner.next();
     }
 
     private static void encryptMessage(String strategy, String input) {
         for (var cipher : getCipher(strategy))
             System.out.println("Your encrypted message is : " + cipher.cipher(input));
+    }
+
+    private static List<String> printCipher() {
+
+        return ServiceLoader.load(Cipher.class)
+                .stream()
+                .filter(c -> c.type().isAnnotationPresent(EncryptStrategy.class))
+                .map(c -> c.type().getAnnotation(EncryptStrategy.class).value())
+                .toList();
     }
 
     private static List<Cipher> getCipher(String annotationType) {
